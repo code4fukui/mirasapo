79f495fee65ccba0c4cb38f6e5861eaf05c31c40
Taisuke Fukuno <fukuno@jig.jp>
2022-12-02 06:30:33 +0900

    update data

--- download.js
+++ download.js
@@ -2,8 +2,9 @@ import { unzip1 } from "https://code4fukui.github.io/address-japan/unzip1.js";
 
 const url = "https://catalog.registries.digital.go.jp/rsc/supports.zip";
 const bin = new Uint8Array(await (await fetch(url)).arrayBuffer());
-//await Deno.writeFile("supports.zip", bin);
+await Deno.writeFile("supports.zip", bin);
 console.log(bin);
+
 const org = unzip1(bin);
 console.log(org);
 await Deno.writeFile("supports.json", org);
